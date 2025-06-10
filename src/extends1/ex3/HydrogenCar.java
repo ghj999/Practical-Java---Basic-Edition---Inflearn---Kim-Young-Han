388e6bd39754f2d09fef24cd93c47f9d9afdeb83
Gihong Jeong <jjninefr9@gmail.com>
2025-06-10 19:13:32 +0900

    작업 내용 커밋 메시지

---
+++ src/extends1/ex3/HydrogenCar.java
@@ -0,0 +1,10 @@
+package extends1.ex3;
+
+import extends1.ex2.Car;
+
+public class HydrogenCar extends Car {
+
+        public void fillHydrogen() {
+            System.out.println("수소를 충전합니다.");
+        }
+}
